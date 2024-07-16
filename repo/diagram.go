df2e6fa80b6a56e67d9b3551245b4c528f56a8a3
chu.tq <chu.tq@teko.vn>
2024-07-16 20:27:01 +0700

    add diagram crud

---
+++ repo/diagram.go
@@ -0,0 +1,45 @@
+package repo
+
+import (
+	"tekdraw-bff/model"
+	"tekdraw-bff/pkg"
+)
+
+func GetDiagram(id int) (*model.Diagram, error) {
+	diagram := model.Diagram{}
+	err := pkg.DB.Table("diagrams").Where(
+		"id = ?", id,
+	).First(&diagram).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &diagram, nil
+}
+
+func AddDiagram(userId int, dType string, code string) error {
+	return pkg.DB.Table("diagrams").Create(&model.Diagram{
+		UserId: userId,
+		Type:   dType,
+		Code:   code,
+	}).Error
+}
+
+func UpdateDiagram(id int, request model.UpdateDiagramRequest) error {
+	updateData := map[string]interface{}{}
+	if request.UserId != nil {
+		updateData["user_id"] = *request.UserId
+	}
+	if request.Type != nil {
+		updateData["type"] = *request.Type
+	}
+	if request.Code != nil {
+		updateData["code"] = *request.Code
+	}
+	return pkg.DB.Table("diagrams").Where("id = ?", id).Updates(updateData).Error
+}
+
+func DeleteDiagram(id int) error {
+	return pkg.DB.Table("diagrams").Where("id = ?", id).Delete(&model.Diagram{}).Error
+}
